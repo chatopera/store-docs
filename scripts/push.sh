92fe40a3a238937ec65dc5f18d590472f91d19e8
Hai Liang Wang <hai@chatopera.com>
2023-11-03 14:06:13 +0800

    Update content

---
+++ scripts/push.sh
@@ -0,0 +1,27 @@
+#! /bin/bash 
+###########################################
+#
+###########################################
+
+# constants
+baseDir=$(cd `dirname "$0"`;pwd)
+cwdDir=$PWD
+export PYTHONUNBUFFERED=1
+export PATH=/opt/miniconda3/envs/venv-py3/bin:$PATH
+export TS=$(date +%Y%m%d%H%M%S)
+export DATE=`date "+%Y%m%d"`
+export DATE_WITH_TIME=`date "+%Y%m%d-%H%M%S"` #add %3N as we want millisecond too
+
+# functions
+
+# main 
+[ -z "${BASH_SOURCE[0]}" -o "${BASH_SOURCE[0]}" = "$0" ] || return
+cd $baseDir/..
+
+git add --all
+
+git commit -m "Update content"
+
+git push origin master -f && \
+    git push github master -f && \
+    git push gitcode master -f
