c3b4c04b2038f8b8c3971a65d2cdc1afef59bf26
Hai Liang Wang <hai@chatopera.com>
2023-11-29 18:24:39 +0800

    https://github.com/chatopera/store-docs/issues/1 fix build error for baseUrl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,7 +13,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://readthe.gitee.io/store-docs',
+  url: 'https://readthe.gitee.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/store-docs',
